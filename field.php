60375f62203b407c9d05115c36633035e9528a95
Evgenii Nasyrov <inasyrov@yandex.ru>
2019-06-25 17:03:25 +0100

    Update field.php

--- field.php
+++ field.php
@@ -1,7 +1,19 @@
 <?php
+/*
+Plugin Name:    Carbon Fields Toggler Field Type
+Description:    Carbon Fields extension that adds a select post field type.
+Version:        0.0.0
+Author:         interconnect/it
+Author URI:     https://interconnectit.com/
+*/
+
+// Require Composer auto loader if installed on it's own
+if (file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
+    require_once $composer;
+}
+
 use Carbon_Fields\Carbon_Fields;
 use Carbon_Field_Toggler\Toggler_Field;
-
 
 define( 'Carbon_Field_Toggler\\DIR', __DIR__ );
 
